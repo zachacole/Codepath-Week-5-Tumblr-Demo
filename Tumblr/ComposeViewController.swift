a773b292b986a8ffdfef2ace12cacce4b95d6c80
Zach Cole <zach.a.cole@gmail.com>
2016-02-28 00:06:58 -0800

    Commit 2
    Modal segue to Compose vc, icon selected states, background view color
    for Trending vc.
    
    Time: 3 hours, Total time: 5.5 hours

--- Tumblr/ComposeViewController.swift
+++ Tumblr/ComposeViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    @IBOutlet weak var textIcon: UIImageView!
+    @IBOutlet weak var photoIcon: UIImageView!
+    @IBOutlet weak var quoteIcon: UIImageView!
+    @IBOutlet weak var linkIcon: UIImageView!
+    @IBOutlet weak var chatIcon: UIImageView!
+    @IBOutlet weak var videoIcon: UIImageView!
+    
+    var offset: CGFloat!
+    var textDown: CGPoint!
+    var photoDown: CGPoint!
+    var quoteDown: CGPoint!
+    var linkDown: CGPoint!
+    var chatDown: CGPoint!
+    var videoDown: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        offset = 400
+        textDown = CGPoint(x: textIcon.center.x, y: textIcon.center.y + offset)
+        photoDown = CGPoint(x: photoIcon.center.x, y: photoIcon.center.y + offset)
+        quoteDown = CGPoint(x: quoteIcon.center.x, y: quoteIcon.center.y + offset)
+        linkDown = CGPoint(x: linkIcon.center.x, y: linkIcon.center.y + offset)
+        chatDown = CGPoint(x: chatIcon.center.x, y: chatIcon.center.y + offset)
+        videoDown = CGPoint(x: videoIcon.center.x, y: videoIcon.center.y + offset)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +44,90 @@ class ComposeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(animated: Bool) {
+        textIcon.center = textDown
+        photoIcon.center = photoDown
+        quoteIcon.center = quoteDown
+        linkIcon.center = linkDown
+        chatIcon.center = chatDown
+        videoIcon.center = videoDown
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        UIView.animateWithDuration(0.1) { () -> Void in
+            self.textIcon.transform = CGAffineTransformMakeTranslation(0, -400)
+        }
+        
+        UIView.animateWithDuration(0.18) { () -> Void in
+            self.photoIcon.transform = CGAffineTransformMakeTranslation(0, -400)
+        }
+        
+        UIView.animateWithDuration(0.26) { () -> Void in
+            self.quoteIcon.transform = CGAffineTransformMakeTranslation(0, -400)
+        }
+        
+        UIView.animateWithDuration(0.34) { () -> Void in
+            self.linkIcon.transform = CGAffineTransformMakeTranslation(0, -400)
+        }
+        
+        UIView.animateWithDuration(0.42) { () -> Void in
+            self.chatIcon.transform = CGAffineTransformMakeTranslation(0, -400)
+        }
+        
+        UIView.animateWithDuration(0.50) { () -> Void in
+            self.videoIcon.transform = CGAffineTransformMakeTranslation(0, -400)
+        }
+    }
+    
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+    @IBAction func didTapDismiss(sender: UIButton) {
+        
+        UIView.animateWithDuration(0.1) { () -> Void in
+            self.textIcon.transform = CGAffineTransformMakeTranslation(0, -800)
+        }
+        
+        UIView.animateWithDuration(0.18) { () -> Void in
+            self.photoIcon.transform = CGAffineTransformMakeTranslation(0, -800)
+        }
+        
+        UIView.animateWithDuration(0.26) { () -> Void in
+            self.quoteIcon.transform = CGAffineTransformMakeTranslation(0, -800)
+        }
+        
+        UIView.animateWithDuration(0.34) { () -> Void in
+            self.linkIcon.transform = CGAffineTransformMakeTranslation(0, -800)
+        }
+        
+        UIView.animateWithDuration(0.42) { () -> Void in
+            self.chatIcon.transform = CGAffineTransformMakeTranslation(0, -800)
+        }
+        
+        UIView.animateWithDuration(0.50) { () -> Void in
+            self.videoIcon.transform = CGAffineTransformMakeTranslation(0, -800)
+        }
+        
+        delay(0.6) {
+            self.textIcon.hidden = true
+            self.photoIcon.hidden = true
+            self.quoteIcon.hidden = true
+            self.linkIcon.hidden = true
+            self.chatIcon.hidden = true
+            self.videoIcon.hidden = true
+        }
+        
+        delay(0.8) {
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
 
     /*
     // MARK: - Navigation
--- Tumblr/TabBarViewController.swift
+++ Tumblr/TabBarViewController.swift
@@ -17,11 +17,16 @@ class TabBarViewController: UIViewController {
     @IBOutlet weak var accountButton: UIButton!
     @IBOutlet weak var trendingButton: UIButton!
     
+    
     var homeViewController: UIViewController!
     var searchViewController: UIViewController!
-    var composeViewController: UIViewController!
+   // var composeViewController: UIViewController!
     var accountViewController: UIViewController!
     var trendingViewController: UIViewController!
+    
+    //var viewControllers: [UIViewController]!
+    
+    //viewControllers = [homeViewController, searchViewController, accountViewController, trendingViewController]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +37,13 @@ class TabBarViewController: UIViewController {
         
         homeViewController = storyboard.instantiateViewControllerWithIdentifier("HomeViewController")
         searchViewController = storyboard.instantiateViewControllerWithIdentifier("SearchViewController")
-        composeViewController = storyboard.instantiateViewControllerWithIdentifier("ComposeViewController")
+        //composeViewController = storyboard.instantiateViewControllerWithIdentifier("ComposeViewController")
         accountViewController = storyboard.instantiateViewControllerWithIdentifier("AccountViewController")
         trendingViewController = storyboard.instantiateViewControllerWithIdentifier("TrendingViewController")
+        
+        homeViewController.view.frame = contentView.frame
+        contentView.addSubview(homeViewController.view)
+        homeButton.selected = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,26 +54,38 @@ class TabBarViewController: UIViewController {
     @IBAction func onHomeButton(sender: UIButton) {
         homeViewController.view.frame = contentView.frame
         contentView.addSubview(homeViewController.view)
+        homeButton.selected = true
+        searchButton.selected = false
+        accountButton.selected = false
+        trendingButton.selected = false
     }
     
     @IBAction func onSearchButton(sender: UIButton) {
         searchViewController.view.frame = contentView.frame
         contentView.addSubview(searchViewController.view)
+        homeButton.selected = false
+        searchButton.selected = true
+        accountButton.selected = false
+        trendingButton.selected = false
     }
-    
-    @IBAction func onComposeButton(sender: UIButton) {
-        composeViewController.view.frame = contentView.frame
-        contentView.addSubview(composeViewController.view)
-    }
+
     
     @IBAction func onAccountButton(sender: UIButton) {
         accountViewController.view.frame = contentView.frame
         contentView.addSubview(accountViewController.view)
+        homeButton.selected = false
+        searchButton.selected = false
+        accountButton.selected = true
+        trendingButton.selected = false
     }
     
     @IBAction func onTrendingButton(sender: UIButton) {
         trendingViewController.view.frame = contentView.frame
         contentView.addSubview(trendingViewController.view)
+        homeButton.selected = false
+        searchButton.selected = false
+        accountButton.selected = false
+        trendingButton.selected = true
     }
     
     
